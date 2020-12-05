9d51455f1880a36391e8062c622712e847100067
ChoiDeborah <adorablejimini@gmail.com>
2020-12-05 23:33:04 +0900

    회원 관리 예제 회원 도메인과 레포지토리 만들기

---
+++ inflearn-yhk/spring-intro/src/main/java/me/deborah/springintro/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package me.deborah.springintro.repository;
+
+import me.deborah.springintro.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    // Optional Java 8 문법
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findALl();
+}
---
+++ inflearn-yhk/spring-intro/src/main/java/me/deborah/springintro/repository/MemoryMemberRepository.java
@@ -0,0 +1,38 @@
+package me.deborah.springintro.repository;
+
+import com.sun.source.tree.NewArrayTree;
+import me.deborah.springintro.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    // 키 중복 문제가 있을 수 있으나 예제니까
+    private static Map<Long, Member> store = new HashMap<>();
+    //  0, 1, 2... key value 생성해 줄 변수
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+       member.setId(++sequence);
+       store.put(member.getId(), member);
+       return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter((member -> member.getName().equals(name)))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findALl() {
+            return new ArrayList<>(store.values());
+    }
+}
