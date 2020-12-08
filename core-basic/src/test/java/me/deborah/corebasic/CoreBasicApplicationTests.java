be31c48d983f3196ba69b22e94fd842d2eb5ab52
ChoiDeborah <adorablejimini@gmail.com>
2020-12-08 18:21:46 +0900

    initializing project

---
+++ core-basic/src/test/java/me/deborah/corebasic/CoreBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package me.deborah.corebasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
