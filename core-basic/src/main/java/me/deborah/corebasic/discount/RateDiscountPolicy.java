1e3b6c99b7cf15757b807b40d45839badd2959cb
ChoiDeborah <adorablejimini@gmail.com>
2020-12-09 21:51:51 +0900

    예제2 - 새로운 할인 정책 개발

---
+++ core-basic/src/main/java/me/deborah/corebasic/discount/RateDiscountPolicy.java
@@ -0,0 +1,19 @@
+package me.deborah.corebasic.discount;
+
+import me.deborah.corebasic.member.Grade;
+import me.deborah.corebasic.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+
+    }
+}
