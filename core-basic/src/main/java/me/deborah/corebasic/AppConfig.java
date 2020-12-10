cd4ece0639b86cd8a9fa4e13948509317af19f31
ChoiDeborah <adorablejimini@gmail.com>
2020-12-10 12:47:17 +0900

    예제2 - 새로운 구조와 할인 정책 적용

--- core-basic/src/main/java/me/deborah/corebasic/AppConfig.java
+++ core-basic/src/main/java/me/deborah/corebasic/AppConfig.java
@@ -2,6 +2,7 @@ package me.deborah.corebasic;
 
 import me.deborah.corebasic.discount.DiscountPolicy;
 import me.deborah.corebasic.discount.FixDiscountPolicy;
+import me.deborah.corebasic.discount.RateDiscountPolicy;
 import me.deborah.corebasic.member.MemberRepository;
 import me.deborah.corebasic.member.MemberService;
 import me.deborah.corebasic.member.MemberServiceImpl;
@@ -32,7 +33,10 @@ public class AppConfig {
     }
 
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+       // AppConfig 에서 할인 정책 역할을 담당하는 구현을 FixDiscountPolicy 객체로 변경했다.
+       // 이제 할인 정책을 변경해도, 애플리케이션의 구성 역할을 담당하는 AppConfig만 변경하면 된다. 클라이언 트 코드인 OrderServiceImpl 를 포함해서 사용 영역의 어떤 코드도 변경할 필요가 없다.
+       // 구성 영역은 당연히 변경된다. 구성 역할을 담당하는 AppConfig를 애플리케이션이라는 공연의 기획자로 생각하자. 공연 기획자는 공연 참여자인 구현 객체들을 모두 알아야 한다.
+        return new RateDiscountPolicy();
     }
 
     // 기존 AppConfig를 보면 중복이 있고, 역할에 따른 구현이 잘 안보인다.
