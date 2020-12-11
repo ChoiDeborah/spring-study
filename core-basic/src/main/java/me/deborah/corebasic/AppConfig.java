81bcd6bbad819001104d759864284ef67d816791
ChoiDeborah <adorablejimini@gmail.com>
2020-12-11 21:42:07 +0900

    [싱글톤 컨테이너] @Configuration과 싱글톤

--- core-basic/src/main/java/me/deborah/corebasic/AppConfig.java
+++ core-basic/src/main/java/me/deborah/corebasic/AppConfig.java
@@ -26,16 +26,19 @@ public class AppConfig {
     // 클라이언트인 memberServiceImpl 입장에서 보면 의존관계를 마치 외부에서 주입해주는 것 같다고 해서 DI(Dependency Injection) 우리말로 의존관계 주입 또는 의존성 주입이라 한다.
     @Bean
     public MemberService memberService() {
+        System.out.println("Call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("Call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("Call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
