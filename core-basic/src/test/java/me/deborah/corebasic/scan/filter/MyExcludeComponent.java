9db5ad1d584c8288e346295be9777e7e4862540d
ChoiDeborah <adorablejimini@gmail.com>
2020-12-12 00:02:06 +0900

    수정

--- core-basic/src/test/java/me/deborah/corebasic/scan/filter/MyExcludeComponent.java
+++ core-basic/src/test/java/me/deborah/corebasic/scan/filter/MyExcludeComponent.java
@@ -1,4 +1,9 @@
 package me.deborah.corebasic.scan.filter;
 
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface MyExcludeComponent {
 }
