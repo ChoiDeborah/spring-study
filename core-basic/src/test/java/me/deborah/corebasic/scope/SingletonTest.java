42ad5e78fd81f93eb7e70dd1c9a304070d2d4eae
ChoiDeborah <adorablejimini@gmail.com>
2020-12-12 23:57:09 +0900

    [프로토타입 스코프] 프로토타입 스코프

---
+++ core-basic/src/test/java/me/deborah/corebasic/scope/SingletonTest.java
@@ -0,0 +1,43 @@
+package me.deborah.corebasic.scope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonTest {
+
+    @Test
+    void singletonBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);
+        SingletonBean singletonBean1 = ac.getBean(SingletonBean.class);
+        SingletonBean singletonBean2 = ac.getBean(SingletonBean.class);
+        System.out.println("singletonBean1 = " + singletonBean1);
+        System.out.println("singletonBean2 = " + singletonBean2);
+        assertThat(singletonBean1).isSameAs(singletonBean2);
+
+        ac.close();
+    }
+
+    @Scope("singleton")
+    static class SingletonBean {
+
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("SingletonBean.destroy");
+        }
+    }
+
+    // 빈 초기화 메서드를 실행하고,
+    // 같은 인스턴스의 빈을 조회하고,
+    // 종료 메서드까지 정상 호출 된 것을 확인할 수 있다.
+}
