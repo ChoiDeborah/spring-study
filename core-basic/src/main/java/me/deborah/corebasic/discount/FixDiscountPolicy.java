9a24ab3968e63973808570c2473e5cefa655fcdf
ChoiDeborah <adorablejimini@gmail.com>
2020-12-12 22:11:31 +0900

    [의존관계 자동 주입] @Autowired 필드 명, @Qualifier, @Primary

--- core-basic/src/main/java/me/deborah/corebasic/discount/FixDiscountPolicy.java
+++ core-basic/src/main/java/me/deborah/corebasic/discount/FixDiscountPolicy.java
@@ -2,9 +2,11 @@ package me.deborah.corebasic.discount;
 
 import me.deborah.corebasic.member.Grade;
 import me.deborah.corebasic.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Qualifier("fixDiscountPolicy") // 빈 등록시 @Qualifier를 붙여 준다.
 public class FixDiscountPolicy implements DiscountPolicy{
     private int discountFixAmount = 1000;
 
--- core-basic/src/main/java/me/deborah/corebasic/discount/RateDiscountPolicy.java
+++ core-basic/src/main/java/me/deborah/corebasic/discount/RateDiscountPolicy.java
@@ -2,9 +2,13 @@ package me.deborah.corebasic.discount;
 
 import me.deborah.corebasic.member.Grade;
 import me.deborah.corebasic.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Qualifier("mainDiscountPolicy")    // 빈 등록시 @Qualifier를 붙여 준다.
+@Primary
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
