fd5054628793d47682643fed90333657d91eec9d
ChoiDeborah <adorablejimini@gmail.com>
2020-12-13 02:03:07 +0900

    스코프와 Provider

--- core-basic/src/main/java/me/deborah/corebasic/web/LogDemoController.java
+++ core-basic/src/main/java/me/deborah/corebasic/web/LogDemoController.java
@@ -2,6 +2,7 @@ package me.deborah.corebasic.web;
 
 import lombok.RequiredArgsConstructor;
 import me.deborah.corebasic.common.MyLogger;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,23 +13,18 @@ import javax.servlet.http.HttpServletRequest;
 @RequiredArgsConstructor
 
 public class LogDemoController {
+
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
-
-        // > 참고: requestURL을 MyLogger에 저장하는 부분은 컨트롤러 보다는
-        // 공통 처리가 가능한 스프링 인터셉 터나 서블릿 필터 같은 곳을 활용하는 것이 좋다.
-        // 여기서는 예제를 단순화하고, 아직 스프링 인터셉터를 학습 하지 않은 분들을 위해서 컨트롤러를 사용했다.
-        // 스프링 웹에 익숙하다면 인터셉터를 사용해서 구현해보자.
-
         myLogger.log("controller test");
-        logDemoService.logic("testId");
-        return "OK";
+        logDemoService.logic("testId"); return "OK";
     }
 
     // 기대하는 출력
@@ -45,4 +41,9 @@ public class LogDemoController {
     // 스프링 애플리케이션을 실행 시키면 오류가 발생한다. 메시지 마지막에 싱글톤이라는 단어가 나오고...
     // 스프링 애플리케이션을 실행하는 시점에 싱글톤 빈은 생성해서 주입이 가능하지만,
     // request 스코프 빈은 아직 생성되지 않는다. 이 빈은 실제 고객의 요청이 와야 생성할 수 있다!
+
+    // ObjectProvider 덕분에 ObjectProvider.getObject() 를 호출하는 시점까지 request scope 빈의 생성을 지연할 수 있다.
+    // ObjectProvider.getObject() 를 호출하시는 시점에는 HTTP 요청이 진행중이므로 request scope 빈의 생성이 정상 처리된다.
+    // ObjectProvider.getObject() 를 LogDemoController , LogDemoService 에서 각각 한번씩 따로 호 출해도
+    // 같은 HTTP 요청이면 같은 스프링 빈이 반환된다! 내가 직접 이걸 구분하려면 얼마나 힘들까 ᅲ ᅲ...
 }
