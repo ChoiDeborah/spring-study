7bc06d35c769417cd6097e0b4c27647c13a129ca
ChoiDeborah <adorablejimini@gmail.com>
2020-12-13 13:23:12 +0900

    Initializing project

---
+++ using-jpa-1/src/main/java/me/deborah/usingjpa1/UsingJpa1Application.java
@@ -0,0 +1,19 @@
+package me.deborah.usingjpa1;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Helper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Set;
+
+@SpringBootApplication
+public class UsingJpa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsingJpa1Application.class, args);
+	}
+
+
+}
