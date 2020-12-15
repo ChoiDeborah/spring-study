4cf253ebba81fcdeb9fcdeb545f8f6230a6c2ddc
ChoiDeborah <adorablejimini@gmail.com>
2020-12-15 20:54:43 +0900

    [회원 도메인 개발] 회원 서비스 개발

--- using-jpa-1/src/main/java/me/deborah/usingjpa1/Repository/MemberRepository.java
+++ using-jpa-1/src/main/java/me/deborah/usingjpa1/Repository/MemberRepository.java
@@ -1,5 +1,6 @@
 package me.deborah.usingjpa1.Repository;
 
+import lombok.RequiredArgsConstructor;
 import me.deborah.usingjpa1.domain.Member;
 import org.springframework.stereotype.Repository;
 
@@ -8,10 +9,15 @@ import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
     // 엔티티 메니저( EntityManager ) 주입  //@PersistenceUnit : 엔티티 메니터 팩토리( EntityManagerFactory ) 주입
-    @PersistenceContext
-    private EntityManager em;
+    //@PersistenceContext   스프링 부트가 @Autowired로 인젝션 되도록 지원해줌.
+    private final EntityManager em;
+
+    //public MemberRepository(EntityManager em) {
+    //    this.em = em;
+    //}
 
     public Long save(Member member) {
         em.persist(member);
