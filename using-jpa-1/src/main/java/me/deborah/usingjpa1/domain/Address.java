bea9f639b590e6399050fed1c6cd9bac65bc22e0
ChoiDeborah <adorablejimini@gmail.com>
2020-12-15 18:43:01 +0900

    [도메인 분석 설계] 엔티티 설계 시 주의점

--- using-jpa-1/src/main/java/me/deborah/usingjpa1/domain/Address.java
+++ using-jpa-1/src/main/java/me/deborah/usingjpa1/domain/Address.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 import javax.persistence.Embeddable;
 
 @Embeddable
-@Getter
+@Getter // 엔티티에는 가급적 Setter를 사용하지 말자
+        // Setter가 모두 열려있다. 변경 포인트가 너무 많아서, 유지보수가 어렵다. 나중에 리펙토링으로 Setter 제거
 public class Address {
     private String city;
     private String street;
--- using-jpa-1/src/main/java/me/deborah/usingjpa1/domain/Order.java
+++ using-jpa-1/src/main/java/me/deborah/usingjpa1/domain/Order.java
@@ -17,14 +17,24 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    // JPQL select o From order o; -> SQL select * from order 100(n) + 1 (order)
+    // - 모든 연관관계는 지연로딩으로 설정!
+    // 즉시로딩( EAGER )은 예측이 어렵고, 어떤 SQL이 실행될지 추적하기 어렵다. 특히 JPQL을 실행할 때 N+1 문제가 자주 발생한다.
+    // 실무에서 모든 연관관계는 지연로딩( LAZY )으로 설정해야 한다.
+    // 연관된 엔티티를 함께 DB에서 조회해야 하면, fetch join 또는 엔티티 그래프 기능을 사용한다.
+    // @XToOne(OneToOne, ManyToOne) 관계는 기본이 즉시로딩이므로 직접 지연로딩으로 설정해야 한다.
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    // persist(orderItemA)
+    // persist(orderItemB)
+    // persist(orderItemC) 를
+    // persist(order) 으로 가능 (cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -32,4 +42,21 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문 상태 [ORDER, CANCLE]
+
+    //== 연관관계 메서드 ==//
+    // 위치는 컨트롤 하는 쪽이 들고 있는 게 좋음.
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        this.orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
