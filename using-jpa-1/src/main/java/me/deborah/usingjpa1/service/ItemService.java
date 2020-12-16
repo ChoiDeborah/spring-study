36b753cea08d111c85a397b1ede94396cdf8090a
ChoiDeborah <adorablejimini@gmail.com>
2020-12-16 18:48:23 +0900

    [주문 도메인 개발] 주문, 주문상품 엔티티 개발

--- using-jpa-1/src/main/java/me/deborah/usingjpa1/service/ItemService.java
+++ using-jpa-1/src/main/java/me/deborah/usingjpa1/service/ItemService.java
@@ -1,2 +1,30 @@
-package me.deborah.usingjpa1.service;public class ItemService {
+package me.deborah.usingjpa1.service;
+
+import lombok.RequiredArgsConstructor;
+import me.deborah.usingjpa1.domain.item.Item;
+import me.deborah.usingjpa1.repository.ItemRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
 }
