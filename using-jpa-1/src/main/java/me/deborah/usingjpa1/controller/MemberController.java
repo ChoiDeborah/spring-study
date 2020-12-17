c16667e9c48ee1b8132f74fecdeb83e8af42b00a
ChoiDeborah <adorablejimini@gmail.com>
2020-12-17 23:52:29 +0900

    [웹 계층 개발] 회원 목록 조회

--- using-jpa-1/src/main/java/me/deborah/usingjpa1/controller/MemberController.java
+++ using-jpa-1/src/main/java/me/deborah/usingjpa1/controller/MemberController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -41,4 +42,19 @@ public class MemberController {
         memberService.join(member);
         return "redirect:/";
     }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
+    // - 폼 객체 vs 엔티티 직접 사용
+    // 요구사항이 정말 단순할 때는 폼 객체( MemberForm ) 없이 엔티티( Member )를 직접 등록과 수정 화면에서 사용해도 된다.
+    // 하지만 화면 요구사항이 복잡해지기 시작하면, 엔티티에 화면을 처리하기 위한 기능이 점점 증가한다.
+    // 결과적으로 엔티티는 점점 화면에 종속적으로 변하고, 이렇게 화면 기능 때문에 지저분해진 엔티티는 결국 유지보수하기 어려워진다.
+    // 실무에서 엔티티는 핵심 비즈니스 로직만 가지고 있고, 화면을 위한 로직은 없어야 한다.
+    // 화면이나 API에 맞 는 폼 객체나 DTO를 사용하자. 그래서 화면이나 API 요구사항을 이것들로 처리하고,
+    // 엔티티는 최대한 순수 하게 유지하자.
 }
