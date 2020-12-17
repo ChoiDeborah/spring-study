6f32e64a3ee84bcf055d1a757eb659c8faf38ce9
ChoiDeborah <adorablejimini@gmail.com>
2020-12-18 02:25:36 +0900

    [웹 계층 개발] 변경 감지와 병합(merge)

--- using-jpa-1/src/main/java/me/deborah/usingjpa1/controller/ItemController.java
+++ using-jpa-1/src/main/java/me/deborah/usingjpa1/controller/ItemController.java
@@ -69,17 +69,34 @@ public class ItemController {
     }
 
     @PostMapping("items/{itemId}/edit")
-    public String updateItemForm(@ModelAttribute("form") BookForm form) {
-        Book book = new Book();
+    public String updateItem(@PathVariable Long itemId, @ModelAttribute("form") BookForm form) {
+        // 준영속 엔티티?
+        // 영속성 컨텍스트가 더는 관리하지 않는 엔티티를 말한다.
+        // 여기서는 itemService.saveItem(book) 에서 수정을 시도하는 Book 객체다.
+        // Book 객체는 이미 DB 에 한번 저장되어서 식별자가 존재한다.
+        // 이렇게 임의로 만들어낸 엔티티도 기존 식별자를 가지고 있으면 준 영속 엔티티로 볼 수 있다.
 
-        book.setId(form.getId());
-        book.setName(form.getName());
-        book.setPrice(form.getPrice());
-        book.setStockQuantity(form.getStockQuantity());
-        book.setAuthor(form.getAuthor());
-        book.setIsbn(form.getIsbn());
+        // 문제 ? JPA가 관리하지 않기 때문에 아무리 값을 바꿔도 업데이트가 안됨.
+        // 준영속 엔티티를 수정하는 2가지 방법 변경 감지 기능 사용
+        // 병합( merge ) 사용
 
-        itemService.saveItem(book);
+        // book.setId(form.getId());
+        // book.setName(form.getName());
+        // book.setPrice(form.getPrice());
+        // book.setStockQuantity(form.getStockQuantity());
+        // book.setAuthor(form.getAuthor());
+        // book.setIsbn(form.getIsbn());
+
+        // itemService.saveItem(book); // 결과적으로 itemRepository에서 merge 호출
+
+        // - 가장 좋은 해결 방법
+        // 엔티티를 변경할 때는 항상 변경 감지를 사용하세요
+        // 컨트롤러에서 어설프게 엔티티를 생성하지 마세요.
+        // 트랜잭션이 있는 서비스 계층에 식별자( id )와 변경할 데이터를 명확하게 전달하세요.(파라미터 or 넘겨줘야하는 파라미터가 많으면 UpdateItemDataTransObject)
+        // 트랜잭션이 있는 서비스 계층에서 영속 상태의 엔티티를 조회하고, 엔티티의 데이터를 직접 변경하세요.
+        // 트랜잭션 커밋 시점에 변경 감지가 실행됩니다.
+
+        itemService.updateItem(itemId, form.getName(), form.getPrice(), form.getStockQuantity());
         return "redirect:/items";
     }
 }
--- using-jpa-1/src/main/java/me/deborah/usingjpa1/service/ItemService.java
+++ using-jpa-1/src/main/java/me/deborah/usingjpa1/service/ItemService.java
@@ -1,6 +1,7 @@
 package me.deborah.usingjpa1.service;
 
 import lombok.RequiredArgsConstructor;
+import me.deborah.usingjpa1.domain.item.Book;
 import me.deborah.usingjpa1.domain.item.Item;
 import me.deborah.usingjpa1.repository.ItemRepository;
 import org.springframework.stereotype.Service;
@@ -18,6 +19,24 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+        // Id를 기반으로 실제 DB의 영속성 Entity를 찾아
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setName(name); // 예제니 setter사용 안좋음
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
+
+        // - 변경 감지 기능 사용 (추천)
+        // 트랜잭션 안에서 엔티티를 다시 조회,
+        // 변경할 값 선택 트랜잭션 커밋 시점에 변경 감지(Dirty Checking) 이 동작해서
+        // 데이터베이스에 UPDATE SQL 실행
+        // (Transactional의해 커밋 됨. JPA는 flush 날림 (영속성인 애들 중에 변경된 애들 찾아서 업데이트 쿼리 날림))
+
+        // setter 지우고 매소드를 사용해서 업데이트가 일어나는 것을 명시적으로 해주는 것이 좋음.
+        // 값이 어디서 바뀌는지 모른다......!
     }
 
     public List<Item> findItems() {
