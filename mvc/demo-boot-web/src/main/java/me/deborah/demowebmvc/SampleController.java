a2885a867e29878f16a3174791b45d35b71ef0cd
ChoiDeborah <adorablejimini@gmail.com>
2020-12-29 20:53:37 +0900

    [스프링 MVC 활용] 요청 맵핑하기 4부 헤더와 매개변수

--- mvc/demo-boot-web/src/main/java/me/deborah/demowebmvc/SampleController.java
+++ mvc/demo-boot-web/src/main/java/me/deborah/demowebmvc/SampleController.java
@@ -1,5 +1,6 @@
 package me.deborah.demowebmvc;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +9,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class SampleController {
 
-
-    @RequestMapping(
-            value = "/hello",
-            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,       // 얘가 쓰임. 메소드가 오버라이딩 함. 둘다 쓰이는 거 아님.
-            produces = MediaType.TEXT_PLAIN_VALUE
-    )
+//    @RequestMapping(
+//            value = "/hello",
+//            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,       // 얘가 쓰임. 메소드가 오버라이딩 함. 둘다 쓰이는 거 아님.
+//            produces = MediaType.TEXT_PLAIN_VALUE
+//    )
 
     //-  특정한 타입의 데이터를 담고 있는 요청만 처리하는 핸들러
     // @RequestMapping(consumes=MediaType.APPLICATION_JSON_UTF8_VALUE)
@@ -26,6 +26,26 @@ public class SampleController {
     // 매치 되지 않는 경우 406 Not Acceptable 응답
 
     // 클래스에 선언한 @RequestMapping에 사용한 것과 조합이 되지 않고 메소드에 사용한 @RequestMapping 의 설정으로 덮어쓴다.
+
+
+    // - 헤더와 매개변수
+    // 특정한 헤더가 있는 요청을 처리하고 싶은 경우
+    // @RequestMapping(headers = “key”)
+    // 특정한 헤더가 없는 요청을 처리하고 싶은 경우
+    // @RequestMapping(headers = “!key”)
+    // 특정한 헤더 키/값이 있는 요청을 처리하고 싶은 경우
+    // @RequestMapping(headers = “key=value”)
+    // 특정한 요청 매개변수 키를 가지고 있는 요청을 처리하고 싶은 경우
+    // @RequestMapping(params = “a”)
+    // 특정한 요청 매개변수가 없는 요청을 처리하고 싶은 경우
+    // @RequestMapping(params = “!a”)
+    // 특정한 요청 매개변수 키/값을 가지고 있는 요청을 처리하고 싶은 경우
+    // @RequestMapping(params = “a=b”)
+
+//    @RequestMapping(value = "/hello", headers = HttpHeaders.AUTHORIZATION + "=" + "111")
+
+    @RequestMapping(value = "/hello", params = "name")
+
     @ResponseBody
     public String hello() {
         return "hello";
--- mvc/demo-boot-web/src/test/java/me/deborah/demowebmvc/SampleControllerTest.java
+++ mvc/demo-boot-web/src/test/java/me/deborah/demowebmvc/SampleControllerTest.java
@@ -3,6 +3,7 @@ package me.deborah.demowebmvc;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -22,17 +23,25 @@ class SampleControllerTest {
 
     @Test
     public void helloTest() throws Exception {
-       mockMvc.perform(
-               get("/hello")
-               .contentType(MediaType.APPLICATION_JSON_UTF8)
-               .accept(MediaType.APPLICATION_JSON)
-       )
-               .andDo(print())
-               //.andExpect(status().isUnsupportedMediaType()) // 지원하지 않는 Media Type 415 Code
-               .andExpect(status().isNotAcceptable())
-       ;
+//       mockMvc.perform(
+//               get("/hello")
+//               .contentType(MediaType.APPLICATION_JSON_UTF8)
+//               .accept(MediaType.APPLICATION_JSON)
+//       )
+//               .andDo(print())
+//               //.andExpect(status().isUnsupportedMediaType()) // 지원하지 않는 Media Type 415 Code
+//               .andExpect(status().isNotAcceptable())
+//       ;
 
-       //
+       // 헤더와 매개변수
+
+        mockMvc.perform(get("/hello")
+                    //.header(HttpHeaders.AUTHORIZATION, "111")
+                    .param("name", "mozzi")
+        )
+                .andDo(print())
+                .andExpect(status().isOk())
+        ;
     }
 
 
