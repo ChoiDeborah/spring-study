b4d54e8034b24c9a7ebd4f6d73d7acf544b8904b
ChoiDeborah <adorablejimini@gmail.com>
2020-12-29 21:30:50 +0900

    [스프링 MVC 활용] 요청 맵핑하기 5부 HEAD와 OPTIONS

--- mvc/demo-boot-web/src/main/java/me/deborah/demowebmvc/SampleController.java
+++ mvc/demo-boot-web/src/main/java/me/deborah/demowebmvc/SampleController.java
@@ -3,6 +3,8 @@ package me.deborah.demowebmvc;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -44,11 +46,15 @@ public class SampleController {
 
 //    @RequestMapping(value = "/hello", headers = HttpHeaders.AUTHORIZATION + "=" + "111")
 
-    @RequestMapping(value = "/hello", params = "name")
-
+    @GetMapping(value = "/hello")
     @ResponseBody
     public String hello() {
         return "hello";
     }
 
+    @PostMapping("/hello")
+    @ResponseBody
+    public String helloPost() {
+        return "hello";
+    }
 }
--- mvc/demo-boot-web/src/test/java/me/deborah/demowebmvc/SampleControllerTest.java
+++ mvc/demo-boot-web/src/test/java/me/deborah/demowebmvc/SampleControllerTest.java
@@ -10,6 +10,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.awt.*;
 import java.rmi.server.ExportException;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.hasItems;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -35,12 +37,33 @@ class SampleControllerTest {
 
        // 헤더와 매개변수
 
-        mockMvc.perform(get("/hello")
-                    //.header(HttpHeaders.AUTHORIZATION, "111")
-                    .param("name", "mozzi")
-        )
+//        mockMvc.perform(get("/hello")
+//                    //.header(HttpHeaders.AUTHORIZATION, "111")
+//                    .param("name", "mozzi")
+//        )
+//                .andDo(print())
+//                .andExpect(status().isOk())
+//        ;
+
+        // HEAD와 OPTIONS 요청 처리
+
+        // 우리가 구현하지 않아도 스프링 웹 MVC에서 자동으로 처리하는 Method
+        // - HEAD
+        // : GET 요청과 동일하지만 응답 본문을 받아오지 않고 응답 헤더만 받아온다.
+        // - OPTIONS
+        //  : 사용할 수 있는 HTTP Method 제공
+        //    서버 또는 특정 리소스가 제공하는 기능을 확인할 수 있다.
+        //    서버는 Allow 응답 헤더에 사용할 수 있는 HTTP Method 목록을 제공해야 한다.
+
+        mockMvc.perform(options("/hello"))
                 .andDo(print())
                 .andExpect(status().isOk())
+                .andExpect(header().stringValues(HttpHeaders.ALLOW,
+                        // 문자열 순서 상관 없도록
+                        hasItems(containsString("GET"),
+                                containsString("POST"),
+                                containsString("OPTIONS"),
+                                containsString("HEAD"))))
         ;
     }
 
