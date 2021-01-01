e18927711a8d37664f52a74fe1c27e7f0c244e46
ChoiDeborah <adorablejimini@gmail.com>
2021-01-01 13:35:15 +0900

    [스프링 MVC 활용] 핸들러 메소드 13부 MultipartFile

---
+++ mvc/demo-boot-web/src/test/java/me/deborah/demowebmvc/FileControllerTest.java
@@ -0,0 +1,36 @@
+package me.deborah.demowebmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.multipart.MultipartFile;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class FileControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void fileUploadTest() throws Exception {
+        MockMultipartFile file = new MockMultipartFile(
+                "file",
+                "test.txt",
+                "text/plain",
+                "hello file".getBytes());
+
+        this.mockMvc.perform(multipart("/file").file(file))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection());
+    }
+}
