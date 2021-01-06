9728ca024f4ff638ff9bc831a501a1b862c82bf2
ChoiDeborah <adorablejimini@gmail.com>
2021-01-06 17:33:23 +0900

    JPQL 사용

--- jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -17,21 +18,32 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 생성
-//            Member member = new Member();
-//            member.setId(2L);
-//            member.setName("mozzii");
-//            em.persist(member);
+            // JPQL
+            // JAP를 사용하면 엔티티 객체를 중심으로 개발
+            // 문제는 검색 쿼리
+            // 검색을 할 때도 테이블이 아닌 엔티티 객체를 대상으로 검색
+            // 모든 DB 데이터를 객체로 변환해서 검색하는 것은 불가능
+            // 애플리케이션이 필요한 데이터만 DB에서 불러오려면 결국 검색 조건이 포함된 SQL이 필요
 
-            // 조회
-            Member findMember = em.find(Member.class, 1L);
-            //System.out.println("findMember Name = " + findMember.getName());
+            // JAP는 SQL을 추상화한 JPQL이라는 객체 지향 쿼리 언어 제공
+            // SQL과 문법 유사, SELECT, FROM, WHERE, GROUP BY, HAVING, JOIN 지원
 
-            // 수정
-            findMember.setName("zzimo");
+            // 차이?
+            // JPQL은 엔티티 객체를 대상으로 쿼리
+            // SQL은 데이터베이스 테이블을 대상으로 쿼리
 
-            // 삭제
-            //em.remove(findMember);
+            // 장점?
+            // 테이블이 아닌 객체를 대상으로 검색하는 객체 지향 쿼리
+            // SQL을 추상화 해서 특정 데이터베이스 SQL에 의존 X
+
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(8)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member name = " + member.getName());
+            }
 
             tx.commit();
         } catch (Exception e) {
